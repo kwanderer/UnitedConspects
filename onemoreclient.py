6c1915a2f52cd6501ee522ea4d6f1f8b485f9461
Kristine <31922131+krisypon@users.noreply.github.com>
2019-02-19 22:57:33 +0200

    the good one 19/02

---
+++ onemoreclient.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+# client.py  
+import socket
+
+# create a socket object
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
+
+# get local machine name
+# local host IP '127.0.0.1' 
+##host = socket.gethostname()
+host = "127.0.0.1"
+# Define the port on which you want to connect 
+port = 9999
+
+
+# connection to hostname on the port.
+s.connect((host, port)) 
+
+while True:
+    
+# message you send to server 
+    message = input("Enter message or 'quit' -> ")
+
+
+    while message != 'quit':
+    # message sent to server 
+        s.send(message.encode('ascii'))
+
+    # Receive no more than 1024 bytes
+        data = s.recv(1024)    
+    
+    # print the received message 
+        print("The time got from the server is %s" % data.decode('ascii'))
+    
+        break
+    
+# ask the client whether he wants to continue 
+    ans = input('\nDo you want to continue(y/n) :') 
+    if ans == 'y': 
+        continue
+    else: 
+        break
+    
+    
+# close the connection when loop will end
+s.close()
