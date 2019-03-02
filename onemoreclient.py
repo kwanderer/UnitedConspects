11712a263daaae19a77dd2164e0716050cae6f5d
Kristine <31922131+krisypon@users.noreply.github.com>
2019-03-03 00:04:48 +0200

    Update onemoreclient.py

--- onemoreclient.py
+++ onemoreclient.py
@@ -14,7 +14,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ##host = socket.gethostname()
 host = "127.0.0.1"
 # Define the port on which you want to connect 
-port = 9999
+port = 10000
 
 
 # connection to hostname on the port.
