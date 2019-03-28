409c962a2009a1ed042519a080af8e399a34a402
Kristine <31922131+krisypon@users.noreply.github.com>
2019-03-28 16:45:47 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,52 +1,82 @@
-#!/usr/bin/env python3
-
-# client.py  
+#!/usr/bin/python3
+from tkinter import *
 import socket
-import ftplib
-import urllib
+#import PyPDF2
 
-# create a socket object
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+csFT = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+pdf_file = 'my.pdf'
 
-# get local machine name
-# local host IP '127.0.0.1' 
-##host = socket.gethostname()
-host = "127.0.0.1"
-# Define the port on which you want to connect 
-port = 10000
-
-
-# connection to hostname on the port.
-s.connect((host, port)) 
-
-while True:
+class Window(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)                 
+        self.master = master
+        self.init_window()
+        #self.recaiveFile()
+        return 
     
-# message you send to server 
-    message = input("Enter message or 'quit' -> ")
+    def init_window(self):
+        self.master.title("United Conspects") 
+        self.pack(fill=BOTH, expand=1)
 
+        #Show Introducing Text
+        self.showText()
+        #Conect to Server
+        self.conect()
+        #pdf_file = 'My.pdf'        
+        
+        #Exit Button
+        quitButton = Button(self, text="Exit",command=self.client_exit)
+        quitButton.place(x=0, y=100)
+        
+        #Input Message and Send It
+        self.entry = Entry(self)
+        self.button = Button(self, text="Get", command=self.on_button)
+        self.button.place(x=50, y=100)
+        self.entry.place(x=100, y=50)          
+            
+    def showText(self):
+        text = Label(self, text="Welcome to our program! Please, write the file name.")
+        text.pack()
+        return
 
-    while message != 'quit':
-    # message sent to server 
-        s.send(message.encode('ascii'))
-        print("message send")
-
-    # Receive no more than 1024 bytes
-        data = s.recv(1024)
-        print("message receive")
+    def client_exit(self):
+        exit()
+        return
+        
+    def conect(self):
+        csFT.connect((socket.gethostname(), 10000))
+        return
     
-    # print the received message 
-        print("The time got from the server is %s" % data.decode('ascii'))
-    
-        break
-    
-# ask the client whether he wants to continue 
-    ans = input('\nDo you want to continue(y/n) :') 
-    if ans == 'y': 
-        continue
-    else: 
-        break
-    
-    
-# close the connection when loop will end
-s.close()
+    def on_button(self):
+        message=self.entry.get()
+        csFT.send(message.encode('ascii'))
+        self.recaiveFile()
+        return
+        #print(a)
+        
+    #Receive, output and save file    
+    def recaiveFile(self):        
+        with open(pdf_file, "wb") as fw:
+            #print("Receiving..")
+            while True:
+                #print('receiving')
+                data = csFT.recv(1024)
+                if data == b'BEGIN':
+                    continue
+                elif data == b'ENDED':
+                    #print('Breaking from file write')
+                    break
+                else:
+                    fw.write(data)
+                    #print('Wrote to file')
+        fw.close()
+        print("Received..")
+        return
+            
+        
+root = Tk()
+root.geometry("400x300")
+app = Window(root)
+        #return app
+root.mainloop() 
